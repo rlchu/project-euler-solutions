84804892da2e5493fab57319b1df4981eb64d424
bombinthebeehive <ryan.chupick@gmail.com>
2014-01-11 01:56:19 -0500

    addition to euler11.js

--- javascript/euler119.js
+++ javascript/euler119.js
@@ -1,7 +1,6 @@
 //These solutions to project euler challenges by no means illustrate graceful or robust code, but are mostly hack-jobs intent on simply reaching the answer!!
 /* to be run in Node */
 
-
 var a = 10,
 	sum_of_digits = 0,
 	ith_root=0;
@@ -17,21 +16,14 @@ function sortNumber(a,b)
   return a - b;
 }
 
-
 for (base = 2; base < 205891132094649; base ++) {
 	for (exp = 2; exp < 15; exp++) {
 		if( base == digit_sum( Math.pow(base,exp) )){
 			answers.push (  Math.pow(base,exp) );
 		}
-
-
 	}
 	
 	if (answers.length > 31) {break;}
-
-
 }
-
-
 
 console.log( "Our answer (A_30) would have to be: " + answers.sort(sortNumber)[28]);
